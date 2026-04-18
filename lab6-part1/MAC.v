8338c4605e5b7bc0f4e996ead00078d467b57dff
philipxunwu <pwuxun@gmail.com>
2026-04-17 21:38:21 -0400

    m

--- lab6-part1/MAC.v
+++ lab6-part1/MAC.v
@@ -53,9 +53,9 @@ module mac #(
         end
     end
 
-    // Tap the delay line at MULT_LAT to sync with the multiplier output
-    wire delayed_rst    = (MULT_LAT > 0) ? rst_acc_delay[MULT_LAT] : rst_accumulator_in;
-    wire delayed_stream = (MULT_LAT > 0) ? stream_rdy_delay[MULT_LAT] : stream_out_rdy_in;
+    // Tap the delay line at MULT_LAT-1 to sync with the multiplier output
+    wire delayed_rst    = (MULT_LAT > 0) ? rst_acc_delay[MULT_LAT-1] : rst_accumulator_in;
+    wire delayed_stream = (MULT_LAT > 0) ? stream_rdy_delay[MULT_LAT-1] : stream_out_rdy_in;
 
     // Multiplier Instantiation
     multiplier #(
